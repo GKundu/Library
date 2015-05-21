47a0f80f45aa5bd5320d0359847625a3ba0b59b7
Killer <Killer@Killer-PC>
2015-05-20 22:57:19 -0400

    HW  Library Completed

--- Library/src/lab1/Book.java
+++ Library/src/lab1/Book.java
@@ -2,7 +2,8 @@ package lab1;
 
 /**
  * A representation of a book.
- * @author <a href="mailto:rkhatchadourian@citytech.cuny.edu">Raffi Khatchadourian</a>.
+ *
+ * @author Goutam Kundu & Francisco Latacel.
  */
 public class Book {
 
@@ -10,7 +11,7 @@ public class Book {
      * This book's title.
      */
     private String title;
-    
+
     /**
      * True if this book is borrowed and false otherwise.
      */
@@ -18,50 +19,58 @@ public class Book {
 
     /**
      * Creates a new Book.
+     *
      * @param bookTitle The title of the book that will be created.
      */
     public Book(String bookTitle) {
-        // TODO: Implement this method
+        title = bookTitle;
+        borrowed = false;
     }
-   
-    /** 
+
+    /**
      * Marks this book as rented.
      */
     public void rented() {
-        // TODO: Implement this method
+        borrowed = true;
+        System.out.println("Sorry, This book is rented");
     }
-   
+
     /**
      * Marks this book as not rented.
      */
     public void returned() {
-        // TODO: Implement this method
+        borrowed = false;
+        System.out.println("This book is not rented, you can take it");
     }
-   
+
     /**
      * Returns true if this book is rented, false otherwise.
+     *
      * @return true if this book is rented and false otherwise.
      */
     public boolean isBorrowed() {
-        // TODO: Implement this method
+        return borrowed;
     }
-   
-    /** 
+
+    /**
      * Returns this book's title as a String.
+     *
      * @return This book's title as a String.
      */
     public String getTitle() {
-        // TODO: Implement this method
+        return title;
     }
 
     public static void main(String[] arguments) {
-        // Small test of the Book class
+
         Book example = new Book("The Da Vinci Code");
         System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
         System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-        example.rented();
-        System.out.println("Borrowed? (should be true): " + example.isBorrowed());
         example.returned();
         System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+        System.out.println("Rented? (should be false): " + example.isBorrowed());
+        example.rented();
+        System.out.println("Borrowed? (should be true): " + example.isBorrowed());
+        System.out.println("Rented? (should be true): " + example.isBorrowed());
     }
-} 
+}
--- Library/src/lab1/Library.java
+++ Library/src/lab1/Library.java
@@ -2,10 +2,84 @@ package lab1;
 
 /**
  * A representation of a library of books.
- * @author <a href="mailto:rkhatchadourian@citytech.cuny.edu">Raffi Khatchadourian</a>.
+ *
+ * @author Goutam Kundu & Francisco Latacela
  */
 public class Library {
     // TODO: Add the missing implementation to this class
+
+    String address;
+    java.util.ArrayList<Book> books;
+
+    public static final String openingTime = "9am";
+    public static final String closingTime = "5pm";
+
+    public Library(String Address) {
+        address = Address;
+        books = new java.util.ArrayList<Book>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public static void printOpeningHours() {
+        System.out.println("Libraries are open daily from " + openingTime + " to " + closingTime);
+    }
+
+    public void printAddress() {
+        System.out.println(address);
+    }
+
+    public void printAvailableBooks() {
+        boolean presentBook = false;
+        for (Book book : books) {
+            if (!book.isBorrowed()) {
+                System.out.println(book.getTitle());
+                presentBook = true;
+            }
+        }
+        if (!presentBook) {
+            System.out.println("No book in catalog");
+        }
+    }
+
+    public void borrowBook(String title) {
+        int foundBook = 0;
+        for (Book book : books) {
+            if (book.getTitle().equals(title)) {
+                if (foundBook == 0) {
+                    foundBook = 1;
+                }
+                if (!book.isBorrowed()) {
+                    book.rented();
+                    foundBook = 2;
+                    break;
+                }
+            }
+        }
+        if (foundBook == 0) {
+            System.out.println("Sorry, this book is not in our catalog.");
+        } else if (foundBook == 1) {
+            System.out.println("Sorry, this book is already borrowed.");
+        } else if (foundBook == 2) {
+            System.out.println("You successfully borrowed " + title);
+        }
+    }
+
+    public void returnBook(String title) {
+        boolean found = false;
+        for (Book book : books) {
+            if (book.getTitle().equals(title) && book.isBorrowed()) {
+                book.returned();
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("You successfully returned " + title);
+        }
+    }
 
     public static void main(String[] args) {
         // Create two libraries
